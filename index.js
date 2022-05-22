e6090769532dc339fb1530f02ebe699a6130dfd2
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-05-22 21:13:41 +0600

    get method for tools added

--- index.js
+++ index.js
@@ -11,11 +11,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umln1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-  });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db("manufacturer").collection("tools");
+
+        // items api
+        // get items
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Server is running');
