7983eb7ab1450d4fed8a01362f45aadc1ec17a31
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-05-24 04:22:40 +0600

    put user in database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,18 +13,78 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umln1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("manufacturer").collection("tools");
+        const blogsCollection = client.db("manufacturer").collection("blogs");
+        const purchaseCollection = client.db("manufacturer").collection("purchase");
+        const userCollection = client.db('manufacturer').collection('users');
 
-        // items api
-        // get items
+        // tools api
+        // get tools
         app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
+        });
+
+        // get tools by id
+        app.get('/tools/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        });
+
+        // get blogs
+        app.get('/blogs', async (req, res) => {
+            const query = {};
+            const cursor = blogsCollection.find(query);
+            const blogs = await cursor.toArray();
+            res.send(blogs);
+        });
+
+        // Put User to Database
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        });
+
+        // post purchase
+        app.post('/purchase', async (req, res) => {
+            const purchase = req.body;
+            const query = { toolName: purchase.toolName, unitPrice: purchase.unitPrice, quantity: purchase.quantity, totalPrice: purchase.totalPrice, email: purchase.email, name: purchase.name, phone: purchase.phone, address: purchase.address };
+            const exists = await purchaseCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, purchase: exists });
+            }
+            const result = await purchaseCollection.insertOne(purchase);
+            return res.send({ success: true, result });
         });
 
     } finally {
